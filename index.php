fd6989473d9eee84499e0d1e202f74f9c5d7e7b7
kent.mercado <merken.work@gmail.com>
2023-12-04 13:45:02 +0800

    practice get and post methods

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="style1.css">
 </head>
-
 <body>
-
     <div class="container">
         <h2>Login</h2>
-        <form action="login.php" method="post">
-            <label for="username">Username:</label>
+        <form action="index.php" method="post">
+            <label>Username:</label>
             <input type="text" id="username" name="username" required>
-
-            <label for="password">Password:</label>
+            <label>Password:</label>
             <input type="password" id="password" name="password" required>
-
             <button type="submit">Login</button>
         </form>
     </div>
-
 </body>
-
 </html>
+
+<?php
+   echo "{$_POST["username"]} <br>";
+   echo $_POST["password"];
+?>
